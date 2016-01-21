80963a35085163c1ca91d1d6b22fbee1395089a9
Joseph Karasek and Steve Kinney <skinney103@gmail.com>
2016-01-21 09:18:49 -0800

    add spec countUpTo to count up by input step

--- spec/specs.js
+++ spec/specs.js
@@ -2,4 +2,7 @@ describe('countUpTo', function() {
   it('count up by units of one', function() {
     expect(countUpTo(10, 1)).to.equal("1,2,3,4,5,6,7,8,9,10")
   });
+  it('count up by units equal to the step', function() {
+    expect(countUpTo(10, 2)).to.equal("1,3,5,7,9")
+  });
 });
